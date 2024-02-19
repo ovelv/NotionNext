ac1b957fd84942132c0684c25257ddfdd4d0a480
欧维在创业 <sevenlowe@163.com>
2024-02-19 15:58:08 +0800

    Update blog.config.js
    更新简介

--- blog.config.js
+++ blog.config.js
@@ -19,8 +19,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'LownRain', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'LownRain', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.lownrain.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'LownRain', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://vacuumsealcontainers.lownrain.com/', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'vacuum seal containers', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
